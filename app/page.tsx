c90311f12e37e3e1d0763c60398f1adc00eebcbc
anuragsharan <anuragsharan@gmail.com>
2025-04-04 15:28:16 +0530

    fix: text added 123

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Text from "./components/Text";
 export default function Home() {
   return (
     <>
-      <Text text="Hello, It is a long established that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text" />
+      <Text text="123 Hello, It is a long established that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text" />
       <Button />
     </>
   );
